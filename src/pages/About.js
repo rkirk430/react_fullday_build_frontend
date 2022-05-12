afb65b6e1f8dd08134ad9428ac17f54332f6a77a
rkirk430 <r.kirk430@gmail.com>
2022-05-12 13:40:44 -0400

    Front End Changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,33 +1,33 @@
 import { useState, useEffect } from "react";
 
 function About(props) {
-    // Create State to Hold About Data
-    const [about, setAbout] = useState(null);
+  // create state to hold about data
+  const [about, setAbout] = useState(null);
 
-    // Create function to make API Call
-    const getAboutData = async () => {
-        //Make API Call and Get Response
-        const response = await fetch(props.URL + "about");
-        //Turn Response into Javascript Object
-        const data = await response.json();
-        //Set the About State to the Data
-        setAbout(data);
-    };
+  // create function to make api call
+  const getAboutData = async () => {
+    // make api call and get response
+    const response = await fetch(props.URL + "about");
+    // turn response into javascript object
+    const data = await response.json();
+    // set the about state to the data
+    setAbout(data);
+  };
 
-    //Make an Initial call for the data inside a useEffect, so it only happens once on a component load
-    useEffect(() => getAboutData(), []);
+  // make an initial call for the data inside a useEffect, so it only happens once on component load
+  useEffect(() => getAboutData(), []);
 
-    //Define a function that will return the JSX needed once we get the data
-    const loaded = () => (
-        <div>
-            <h2> {about.name}</h2>
-            <h2> {about.email} </h2>
-            <p> {about.bio} </p>
-        </div>
-    );
+  // define a function that will return the JSX needed once we get the data
+  const loaded = () => (
+    <div>
+      <h2>{about.name}</h2>
+      <h3>{about.email}</h3>
+      <p>{about.bio}</p>
+    </div>
+  );
 
-    //If Data arrives return the result of loaded, if not, an h1 that says loading
-    return about ? loaded() : <h1>Loading...</h1>
-  }
-  
-  export default About;
+  // if data arrives return the result of loaded, if not, an h1 that says loading
+  return about ? loaded() : <h1>Loading...</h1>;
+}
+
+export default About;
