bd17da7b5d4b3e0288f14eba39ccdbe3d4ee5d3c
rkirk430 <r.kirk430@gmail.com>
2022-05-12 16:35:25 -0400

    Addt changes

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-  <Router>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <Router>
       <App />
-  </Router>,
-  document.getElementById("root")
+    </Router>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
